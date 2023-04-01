a6425c9ec9d666c4fa00bff69e55250fe652320a
Joshua <80783216+Joshuii@users.noreply.github.com>
2023-04-01 14:25:27 +0100

    Player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private Camera PlayerCamera;
+
+    public InputAction Move;
+
+    [SerializeField] private float MovementSpeed;
+
+    private Vector3 HeadPosition = Vector3.up;
 
     // Start is called before the first frame update
     void Start() {
@@ -11,6 +18,18 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        Vector2 input = Move.ReadValue<Vector2>().normalized;
+        Vector2 headVelocity = input * MovementSpeed * Time.deltaTime;
+        HeadPosition += new Vector3(headVelocity.x, 0, headVelocity.y);
+
+        transform.position = HeadPosition;
+    }
+
+    private void OnEnable() {
+        Move.Enable();
+    }
+
+    private void OnDisable() {
+        Move.Disable();
     }
 }
