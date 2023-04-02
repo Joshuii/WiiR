51a4ace0b3db9b2a6e6d05ab099b251958e0865e
Alister <80782387+aalister@users.noreply.github.com>
2023-04-02 05:40:54 +0100

    Added glass

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using OpenCover.Framework.Model;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,11 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.collider.TryGetComponent(out Glass glass))
+        {
+            Debug.Log("Glass");
+            glass.Shatter(collision);
+        }
         Destroy(gameObject);
     }
 }
